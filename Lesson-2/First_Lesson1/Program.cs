2eb17d4c813abebefa3b56aedfe8d31ee471ea65
Minaya(Farzali) Aliyeva <41920626+Minayaff@users.noreply.github.com>
2025-02-27 22:59:39 +0400

    Add files via upload

---
+++ Lesson-2/First_Lesson1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace First_Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //reference type : string, arrays
+
+            int[,] arr = new int[3, 3]
+            {
+                {1,2,8 },
+                {4,8,6},
+                {5,6,4}
+
+            };
+            Console.WriteLine(arr[0,2]);
+
+        }
+    }
+
+}
